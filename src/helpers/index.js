36e3fef4f61288ebc1129ac86191561bc27a4647
Kevin Rocker <kevin.j.rocker@gmail.com>
2017-05-19 23:11:58 -0400

    First attempt at Higher Order firebase component

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,41 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
 const _pipe = (f, g) => (...args) => g(f(...args))
 const pipe = (...fns) => fns.reduce(_pipe)
 
+const connectToFirebase(Component, ...sections) {
+  const dbConnection = firebase.database()
+
+  class ConnectComponent extends React.Component {
+    constructor(props) {
+      super(props)
+      const tmp = {}
+      sections.map((str) => tmp[str] = null)
+      this.state = tmp
+    }
+
+    assignState(key, val) {
+      const tmp = {}
+      tmp[key] = val
+      this.setState(tmp)
+    }
+
+    componentWillMount() {
+      sections.map((key) => {
+        dbConnection.ref(key).once('value', (snap) => {
+          this.assignState(key, snap.val())
+        })
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          { Object.values(this.state).every((x) => x === null) ? null :  <Component {...this.props} data={this.state} /> }
+        </div>
+      )
+    }
+  }
+}
 export default { pipe };
