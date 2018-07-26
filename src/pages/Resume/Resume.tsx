dcb5d2b241d1e3003a1eb2fd37034f9e2c94e63a
Kevin Rocker <kevin.j.rocker@gmail.com>
2018-07-25 22:20:21 -0400

    Download Resume Button

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,6 +1,6 @@
 import { TranslationFunction } from 'i18next';
 import * as React from 'react';
-import { Button, Container, ContainerProps } from 'reactstrap';
+import { Container, ContainerProps } from 'reactstrap';
 import { Header } from '../../components';
 import { withText, WithTextProps } from '../../i18next';
 import BodyContent from './BodyContent';
@@ -9,21 +9,26 @@ import ListContent from './ListContent';
 
 export interface ResumeProps extends ContainerProps, WithTextProps {
   title: string;
+  downloadLink: string;
 }
 
-const Resume: React.SFC<ResumeProps> = ({ title, t, i18n, ready, ...rest }) => (
+const Resume: React.SFC<ResumeProps> = ({ title, t, i18n, ready, downloadLink, ...rest }) => (
   <Container {...rest}>
     <Header>{title}</Header>
     <BodyContent id="profile" />
     <JobList />
     <ListContent id="skills" />
     <ListContent id="education" />
+    {/* <hr />
     <Button block={true} href="/resume.pdf" target="__blank">
-      Get the PDF!
-    </Button>
+      {downloadLink}
+    </Button> */}
   </Container>
 );
 
-const mapText = (t: TranslationFunction) => ({ title: t('resume:resumeTitle') });
+const mapText = (t: TranslationFunction) => ({
+  title: t('resume:resumeTitle'),
+  downloadLink: t('resume:downloadLink')
+});
 
 export default withText(mapText)(Resume);
